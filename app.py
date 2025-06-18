9996f708294933fc342ff0665780bf511e96b4e9
Manikanta Nagulapati <99755305+manikantanagulapati3102002@users.noreply.github.com>
2025-06-18 11:58:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request
 import requests
 
-app = Flask(__name__)
+app = Flask(__name__) #START APP
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
